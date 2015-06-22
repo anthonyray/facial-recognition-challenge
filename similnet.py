44c4ac909fd6fa83645d45a7c82e8cbc81205132
anthonyray <dantho361@hotmail.com>
2015-06-22 11:51:37 +0200

    ok ok

--- similnet.py
+++ similnet.py
@@ -518,4 +518,4 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=10000,
     print classifier.logRegressionLayer.p_y_given_x[0].get_value(borrow=True)
 
 if __name__ == '__main__':
-    test_mlp(learning_rate=0.1)
+    test_mlp(learning_rate=0.001)
