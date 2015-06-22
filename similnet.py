d3e63d95e5764de59acb9c6400acacc630fb29ef
anthonyray <dantho361@hotmail.com>
2015-06-22 12:05:06 +0200

    tweaking

--- similnet.py
+++ similnet.py
@@ -517,4 +517,4 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=10000,
     print evaluate_model(y)
 
 if __name__ == '__main__':
-    test_mlp(learning_rate=0.001)
+    test_mlp(n_hidden=1500)
